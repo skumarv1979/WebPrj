503e46f04dabd3f08e2bd3f99ae61b64ba3bb903
Saravanakumar Venkatachalam <skumarv@hotmail.com>
2019-12-22 02:13:01 +0530

    second commit

--- WebPrj/src/main/java/com/skumarv/servlets/SimpleServlet.java
+++ WebPrj/src/main/java/com/skumarv/servlets/SimpleServlet.java
@@ -1,6 +1,8 @@
 package com.skumarv.servlets;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,7 +13,10 @@ import javax.servlet.http.HttpServletResponse;
 import javastrava.api.v3.auth.AuthorisationService;
 import javastrava.api.v3.auth.impl.retrofit.AuthorisationServiceImpl;
 import javastrava.api.v3.auth.model.Token;
+import javastrava.api.v3.auth.model.TokenResponse;
 import javastrava.api.v3.model.StravaAthlete;
+import javastrava.api.v3.rest.API;
+import javastrava.api.v3.rest.AuthorisationAPI;
 import javastrava.api.v3.service.Strava;
 
 /**
@@ -24,11 +29,35 @@ public class SimpleServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/plain");
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		//InputStream input = getServletContext().getResourceAsStream("/application.properties");
+		InputStream input = classLoader.getResourceAsStream("application.properties");
+		
+		Properties prop = new Properties();
+		prop.load(input);
+		System.out.println("strava.client_id :: "+ prop.get("strava.client_id"));
+		int clientId = Integer.parseInt((String) prop.get("strava.client_id"));
+		String clientSecret = prop.get("strava.client_secret").toString();
+		System.out.println("clientId :: "+clientId);
+		System.out.println("clientSecret :: "+clientSecret);
+		//TokenManager tokenManager= TokenManager.instance();
+		//tokenManager.retrieveTokenWithScope("skumarvdlf@gmail.com");
+		//if(tokenManager==null || tokenManager.ge)
 		AuthorisationService service = new AuthorisationServiceImpl();
 		String code = req.getParameter("code");
-		Token token = service.tokenExchange(41792, "41f41b41cada9987e2d856039c42c149e6f2ff75", code);
+		System.out.println("Code :: " + code);
+		//Token token = service.tokenExchange(clientId, clientSecret, code);
+		
+		AuthorisationAPI auth = API.authorisationInstance();
+		TokenResponse response = auth.tokenExchange(clientId, clientSecret, code);
+		Token token = new Token(response);
+		
+		//TokenManager.instance().storeToken(token);
 		Strava strava = new Strava(token);
-		StravaAthlete athlete = strava.getAthlete(761939);
+		StravaAthlete athlete = strava.getAuthenticatedAthlete();
+		System.out.println("First Name :: "+athlete.getFirstname());
+		System.out.println("Last Name :: "+athlete.getLastname());
+		System.out.println("email :: "+athlete.getEmail());
 		resp.getWriter().write("Hello World! Maven Web Project Example.");
 	}
 }
